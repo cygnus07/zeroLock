1f55e978705f9ae3680a9c994545f741cd09a869
Kuldeep Singh <kuldeepsingh21070@gmail.com>
2025-09-23 17:06:43 +0530

    refactor app and server.js files

--- src/app.js
+++ src/app.js
@@ -2,20 +2,44 @@ import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
 import morgan from 'morgan'
+import cookieParser from 'cookie-parser'
+import dotenv from 'dotenv'
 
-const app = express()
-
-app.use(cors())
-app.use(helmet())
-app.use(morgan('dev'))
-app.use(express.urlencoded({
-    extended: true,
-}))
-app.use(express.json())
+dotenv.config()
 
 
-app.get('/', (req,res) => {
-    res.send("Api is running")    
-})
+export const createApp = () => {
+    const app = express()
 
-export default app
+    app.use(cors())
+    app.use(helmet())
+    app.use(express.urlencoded({
+        extended: true,
+        limit: '10mb'
+    }))
+    app.use(express.json({ limit: '10mb'}))
+    app.use(cookieParser())
+
+    if(process.env.NODE_ENV === 'development'){
+        app.use(morgan('dev'))
+    }
+
+
+    app.get('/', (req,res) => {
+        res.status(200).json({
+            status: 'ok',
+            message: 'Api is running'
+        })    
+    })
+
+    app.get('/health', (req,res) => {
+        res.status(200).json({
+            status:'ok',
+            timeStamp: new Date().toISOString(),
+            environment: process.env.NODE_ENV
+        })
+    })
+
+    return app
+
+}
--- src/server.js
+++ src/server.js
@@ -1,8 +1,43 @@
-import app from "./app.js"
+import { createApp } from "./app.js"
+import { createServer} from 'http'
+import dotenv from 'dotenv'
 
+dotenv.config()
 
-const port = process.env.PORT || 4001
-
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`)
+process.on('uncaughtException', (err) => {
+    console.log("Uncaugh Exception: ", err)
+    process.exit(1)
 })
+
+process.on('unhandledRejection', (err) => {
+    console.log('Unhandled Rejection', err)
+    process.exit(1)
+})
+
+
+const startServer = () => {
+    try {
+        const app = createApp()
+        const httpServer = createServer(app)
+        const port = process.env.PORT || 4001
+        httpServer.listen(port, "0.0.0.0", () => {
+            console.log(`
+                Server is running
+                port: ${port}
+                health check: /health`)
+        })
+
+        process.on('SIGTERM', () => {
+            console.log('SIGTERM received, shutting down gracefully')
+            httpServer.close(() =>  {
+                console.log('Server closed')
+                process.exit(0)
+            })
+        })
+    } catch (error) {
+        console.error('Failed to start the server', error)
+        process.exit(1)
+    }
+}
+
+startServer()
